a1eca037cd98b49913dc0946a90db09ba220220c
billwang <billwang2006@outlook.com>
2025-11-12 21:17:54 -0500

    Task 1.1: fix CheckStyle issues in theater classes

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,42 @@
 package theater;
 
+/**
+ * Represents a play, including its name and type.
+ *
+ * @author Letian Wang
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
+    /**
+     * Creates a play with the given name and type.
+     *
+     * @param name the name of the play
+     * @param type the type of the play
+     */
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    /**
+     * Returns the name of this play.
+     *
+     * @return the play name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the type of this play.
+     *
+     * @return the play type
+     */
+    public String getType() {
+        return type;
     }
 }
